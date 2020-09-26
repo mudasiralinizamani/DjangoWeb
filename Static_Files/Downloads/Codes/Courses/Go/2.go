7ba315142aa965ac1c198ac511402589edd4c5e6
Mudasir <mudasiralinizamani@gmail.com>
2020-09-26 19:04:49 +0500

    idk

---
+++ Static_Files/Downloads/Codes/Courses/Go/2.go
@@ -0,0 +1,23 @@
+package main
+
+
+import (
+
+"fmt"
+
+"math"
+
+)
+
+
+func main() {
+
+
+// fmt.Printf("Hello World")
+
+// This line is for printing
+
+fmt.Println("This is from Println")
+
+
+}
---
+++ Static_Files/Downloads/Codes/Courses/Go/4.go
@@ -0,0 +1,19 @@
+package main
+
+
+import (
+
+"fmt"
+
+"reflect"
+
+)
+
+
+func sum(num1, num2 float64) float64 {
+
+
+return num1 + num2
+
+
+}
