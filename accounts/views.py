df50c180f7bfe1af0b1c455e26450880de4f7236
Mudasir <mudasiralinizamani@gmail.com>
2020-10-26 21:04:13 +0500

    accounts views.py is changed

--- accounts/views.py
+++ accounts/views.py
@@ -15,33 +15,41 @@ def Profile(req):
     return render(req, 'Accounts/Admin.html')
 
 def Signup(req):
-    form = forms.CreateUserForm()
-    if req.method == 'POST':
-        form = forms.CreateUserForm(req.POST)
-        if form.is_valid():
-            form.save()
+    if User.is_authenticated():
+        messages.INFO(req, f'You are already Authenticated, {User.username}!')
+        return  redirect('Index')
+    else:
+        form = forms.CreateUserForm()
+        if req.method == 'POST':
+            form = forms.CreateUserForm(req.POST)
+            if form.is_valid():
+                form.save()
 
-            messages.success(req, f'Account was successfully created {form.cleaned_data.get("username")}')
-            return redirect('Login')
-    
-    Content = {'form': form, }
-    return render(req, 'Accounts/Register.html', Content)
+                messages.success(req, f'Account was successfully created {form.cleaned_data.get("username")}')
+                return redirect('Login')
+        
+        Content = {'form': form, }
+        return render(req, 'Accounts/Register.html', Content)
 
 def Login(req):
-    if req.method == 'POST':
-        username = req.POST.get('username')
-        password = req.POST.get('password')
+    if User.is_authenticated():
+        messages.INFO(req, f'You are already Logedin, {User.username}!')
+        return  redirect('Index')
+    else:
+        if req.method == 'POST':
+            username = req.POST.get('username')
+            password = req.POST.get('password')
 
-        user = authenticate(req, username=username, password=password)
+            user = authenticate(req, username=username, password=password)
 
-        if user is not None:
-            login(req, user)
-            messages.success(req, f'Successfully login as, {user}')
-            return redirect('Index')
-        else:
-            messages.error(req, 'username or passoword is incorrect')
+            if user is not None:
+                login(req, user)
+                messages.success(req, f'Successfully login as, {user}')
+                return redirect('Index')
+            else:
+                messages.error(req, 'username or passoword is incorrect')
 
-    return render(req, 'Accounts/Login.html')
+        return render(req, 'Accounts/Login.html')
 
 def Logout(req):
     logout(req)
