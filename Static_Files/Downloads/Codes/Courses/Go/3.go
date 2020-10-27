4ad25a646f629638ed77a388c00d088673d2363d
Mudasir <mudasiralinizamani@gmail.com>
2020-10-27 17:30:02 +0500

    collectstatic

---
+++ Static_Files/Downloads/Codes/Courses/Go/3.go
@@ -0,0 +1,52 @@
+package main
+
+
+import "fmt"
+
+
+func main() {
+
+
+// Boolean Integers Strings Floats
+
+
+x := 15
+
+
+var w = 60
+
+
+w = 90
+
+
+const in = 990
+
+
+var f float64 = 15.2
+
+
+var s string = "Hey there"
+
+
+var b bool = false
+
+
+fmt.Println(x)
+
+
+fmt.Println(f)
+
+
+fmt.Println(s)
+
+
+fmt.Println(b)
+
+
+fmt.Println(w)
+
+
+fmt.Println(in)
+
+
+}
